0c508aa0940109387f2a767151c00c3985e27c29
Joao Gabriel <joaog.oliveeira2@gmail.com>
2026-01-19 11:26:55 -0300

    tela de menu cadastro

--- src/view/CadastroCliente.java
+++ src/view/CadastroCliente.java
@@ -1,18 +1,41 @@
 package view;
 
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 public class CadastroCliente extends JFrame implements ActionListener {
 	
 	
-	
+	private Container container;
+	private JButton bEnviar;
+	private JButton bResetar;
+	private JButton bVoltar;
+	private JLabel nome;
+	private JTextField tNome;
+	private JLabel telefone; 
+	private JTextField tTelefone;
+	private JLabel endereco;
+	private JTextField tEndereco;
+	private JLabel id;
+	private ArrayList cliente;
+	private JTextArea mensagem;
+	 
 
 
 	public CadastroCliente(MenuCadastro menuCadastro) {
-		// TODO Auto-generated constructor stub
+		
+		setTitle("Cadastro de Cliente");
+		setBounds(300, 30, 500, 500);
+		
+		setVisible(true);
 	}
 
 	@Override
@@ -21,4 +44,4 @@ public class CadastroCliente extends JFrame implements ActionListener {
 		
 	}
 
-}
+} 
